4e21f1c21c1460386a2a6a4517f121487cf05fa8
Isaque Ramos Chaves <isaque.chaves@escolaconquer.com.br>
2023-11-09 23:07:59 -0300

    feat: adicionado section Projetos e componente CardProjects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,86 @@
 import CardInfos from '@/components/ui/card-infos'
-import Image from 'next/image'
+import CardProjects from '@/components/ui/card-projects'
 
 export default function Home() {
   return (
     <div className=''>
       <section className='px-28 py-16 flex flex-col justify-between items-center sm:flex-row sm:px-[10%]'>
         <div className='flex flex-col gap-4'>
-          <h1 className='font-bold leading-10'>Olá, eu sou o <br />Isaque Chaves :)</h1>
+          <h1>Olá, eu sou o <br />Isaque Chaves :)</h1>
           <p className=' opacity-70 text-xl'>Desenvolvedor Front-End</p>
           <div className='flex justify-around gap-4'>
             <button className='m-0 p-4 bg-green-500 text-black hover:bg-[#171717] hover:text-white'>Download CSV</button>
             <button className='m-0 p-4 bg-[#171717] border border-[#333333] hover:bg-green-500 hover:text-black'>Entre em Contato</button>
           </div>
         </div>
-        <img src="./isaque-img.png" alt="" className=''/>
+        <img src="./isaque-img.png" alt="" className='' />
       </section>
-      <section className='px-28 p-8 flex flex-col items-center gap-10'>
-        <h1 className='font-bold leading-10'>Sobre mim</h1>
+      <section className='px-28 pt-8 flex flex-col items-center gap-10'>
+        <h1>Sobre mim</h1>
         <p className='opacity-70 text-xl text-center px-20 leading-8'>
           Meu nome é Isaque Ramos Chaves, sou formado em Sistemas de Informação pela Facimp Wyden e atualmente atuo como analista de dados na Escola Conquer. No entanto, estou direcionando minha carreira para o campo do Front-End. Possuo experiência em HTML, CSS, JavaScript, TypeScript, React, Next e Tailwind. Estou ansioso para continuar a minha jornada no universo do Front-End, contribuindo com minha paixão pelo design e usabilidade em projetos digitais.
-          </p>
+        </p>
         <div className='flex justify-around gap-20 py-16'>
-          <CardInfos 
+          <CardInfos
             srcImage="/smile.svg"
             title="Meu Nome"
             subTitle="Isaque Ramos Chaves"
           />
-          <CardInfos 
+          <CardInfos
             srcImage="/mail.svg"
             title="E-mail"
             subTitle="isaque.ramosch@gmail.com"
           />
-          <CardInfos 
+          <CardInfos
             srcImage="/instagram.svg"
             title="Instagram"
             subTitle="@isaquechaves_"
           />
-          <CardInfos 
+          <CardInfos
             srcImage="/phone.svg"
             title="Telefone"
             subTitle="(91) 99391-1310"
+          />
+        </div>
+      </section>
+      <section className='px-28 pt-8 flex flex-col items-center gap-10'>
+        <h1>Projetos</h1>
+        <div className='flex flex-wrap items-center justify-center gap-7'>
+          <CardProjects
+            srcImage='/app-ecommerce.png'
+            alt=''
+            title='App de E-Commerce'
+            subTitle='Tecnilogias: React, Next.js, NextAuth.js (com login via Google), TypeScript, Prisma, PostgreSQL, Tailwind e Stripe'
+          />
+          <CardProjects
+            srcImage="/em-breve.jpg"
+            alt="breve"
+            title="Lorem Ipsum"
+            subTitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry." l
+          />
+          <CardProjects
+            srcImage="/em-breve.jpg"
+            alt="breve"
+            title="Lorem Ipsum"
+            subTitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry." l
+          />
+          <CardProjects
+            srcImage="/em-breve.jpg"
+            alt="breve"
+            title="Lorem Ipsum"
+            subTitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry." l
+          />
+          <CardProjects
+            srcImage="/em-breve.jpg"
+            alt="breve"
+            title="Lorem Ipsum"
+            subTitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry." l
+          />
+          <CardProjects
+            srcImage="/em-breve.jpg"
+            alt="breve"
+            title="Lorem Ipsum"
+            subTitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry." l
           />
         </div>
       </section>
---
+++ src/components/ui/card-projects.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+const CardProjects = ({ srcImage, alt, title, subTitle }: any) => {
+    return (
+        <div className='flex flex-col items-center gap-1 bg-[#212121] p-4 h-[300px] w-[350px]'>
+            <Image
+                src={srcImage}
+                alt={alt}
+                height={130}
+                width={300}
+                className='max-h-[100%] max-w-[100%]'
+            />
+            <div className='flex flex-col items-start'>
+                <h5>{title}</h5>
+                <p className='opacity-70'>{subTitle}</p>
+            </div>
+        </div>
+    );
+}
+
+export default CardProjects;
